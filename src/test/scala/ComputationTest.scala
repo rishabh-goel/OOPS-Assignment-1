d456b388bbb1544fe892987428e3b4187224cf72
rishabhgoel <goel.rishabh12@gmaill.com>
2022-02-12 21:18:25 -0600

    Updated test cases to include code changes for Assign, Scope, Variable

--- src/test/scala/ComputationTest.scala
+++ src/test/scala/ComputationTest.scala
@@ -1,7 +1,9 @@
+package scala
+
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
+import com.rishabh.hw1.Computation.SetExp.{Assign, Cross, Delete, Diff, GetMacro, Insert, Intersect, Scope, SetMacro, Union, Value, Variable}
 
-import scala.Computation.SetExp.{Assign, Cross, Delete, Diff, GetMacro, GetNamedScope, Insert, Intersect, SetAnonScope, SetMacro, SetNamedScope, Union, Value, Variable}
 import scala.collection.mutable
 import scala.collection.mutable.Set
 
@@ -9,72 +11,80 @@ class ComputationTest extends AnyFlatSpec with Matchers{
 
   // Test 1
   it should "Check if sets are created" in {
-    // Create Set A and check the result
-    assert(Assign("A", Value(Set(1,2,3))).eval === mutable.HashMap("A" -> Set(1, 2, 3)))
+    // Create 2 sets A and B in the default scope
+    Assign("A", Value(Set(1,2,3))).eval()
+    Assign("B", Value(Set(4,5,6))).eval()
 
-    // Create Set B and now the map should have both Set A and Set B
-    assert(Assign("B", Value(Set(4,5,6))).eval === mutable.HashMap("A" -> Set(1, 2, 3), "B" -> Set(4, 5, 6)))
+    // Check if variables have been created in the default scope
+    assert(Variable("A").eval() === scala.collection.mutable.HashSet(1, 2, 3))
+    assert(Variable("B").eval() === scala.collection.mutable.HashSet(4, 5, 6))
   }
 
   // Test 2
   it should "Check the result of union and intersection operation" in {
-    // Create a third set E
-    Assign("E", Value(Set(1,5,6))).eval
+    // Create a third set E  in default
+    Assign("E", Value(Set(1,5,6))).eval()
 
-    // Check for Union of 2 sets
-    assert(Union(Variable("A"), Variable("B")).eval === Set(1, 2, 3, 4, 5, 6))
+    // Check for Union of 2 sets in default scope
+    assert(Union(Variable("A"), Variable("B")).eval() === scala.collection.mutable.Set(1, 2, 3, 4, 5, 6))
 
-    // Check for Intersection of 2 sets union with 3rd set
-    assert(Union(Intersect(Variable("A"), Variable("B")), Variable("E")).eval === Set(1, 5, 6))
+    // Check for Intersection of 2 sets union with 3rd set in default scope
+    assert(Union(Intersect(Variable("A"), Variable("B")), Variable("E")).eval() === scala.collection.mutable.Set(1, 5, 6))
   }
 
   // Test 3
   it should "Check the result of difference and intersection operation" in {
-    // Create 2 sets C and D
-    Assign("D", Value(Set(1,5,6))).eval
-    Assign("C", Value(Set(7,8,9))).eval
+    // Create 2 sets C and D  in default scope
+    Assign("D", Value(Set(1,5,6))).eval()
+    Assign("C", Value(Set(7,8,9))).eval()
 
-    // Check Symmetric Difference of set A and D
-    assert(Diff(Variable("A"), Variable("D")).eval === Set(2, 3, 5, 6))
+    // Check Symmetric Difference of set A and D in default scope
+    assert(Diff(Variable("A"), Variable("D")).eval() === scala.collection.mutable.Set(2, 3, 5, 6))
 
-    // Check Catesian Product of set B and C
-    assert(Cross(Variable("B"), Variable("C")).eval === Set((4,7), (4,8), (4,9), (5,7), (5,8), (5,9), (6,7), (6,8), (6,9)))
+    // Check Catesian Product of set B and C in default scope
+    assert(Cross(Variable("B"), Variable("C")).eval() === scala.collection.mutable.Set((4,7), (4,8), (4,9), (5,7), (5,8), (5,9), (6,7), (6,8), (6,9)))
   }
 
   // Test 4
   it should "Check the result of insert and delete with and without macro" in {
 
-    // Delete item from a set without using a macro
-    assert(Delete(Variable("A"), Value(1)).eval === Set(2, 3))
+    // Delete item from a set without using a macro in default scope
+    assert(Delete(Variable("A"), Value(1)).eval() === scala.collection.mutable.Set(2, 3))
 
-    // Insert an item into a set without using a macro
-    assert(Insert(Variable("A"), Value(100)).eval === Set(2, 3, 100))
+    // Insert an item into a set without using a macro in default scope
+    assert(Insert(Variable("A"), Value(100)).eval() === scala.collection.mutable.Set(2, 3, 100))
 
-    // Set Macros for insert and delete operations
-    SetMacro("delete", Delete(Variable("B"), Value(5))).eval
-    SetMacro("insert", Insert(Variable("B"), Value(200))).eval
+    // Set Macros for insert and delete operations in default scope
+    SetMacro("delete", Delete(Variable("B"), Value(5))).eval()
+    SetMacro("insert", Insert(Variable("B"), Value(200))).eval()
 
-    // Compute macros using the GetMacro function
-    assert(GetMacro("delete").eval === Set(4, 6))
-    assert(GetMacro("insert").eval === Set(4, 6, 200))
+    // Compute macros using the GetMacro function in default scope
+    assert(GetMacro("delete").eval() === scala.collection.mutable.Set(4, 6))
+    assert(GetMacro("insert").eval() === scala.collection.mutable.Set(4, 6, 200))
   }
 
   // Test 5
   it should "Check the result of the named and anonymous scope" in {
 
-    // Set inner named scope
-    SetNamedScope("inner", Union(Value(Set(1)), Value(Set(2)))).eval
+    // Create 2 sets A and B  in default scope
+    Assign("A", Value(Set(1,2,3))).eval()
+    Assign("B", Value(Set(4,5,6))).eval()
 
-    // Use inner named scope inside outer named scope
-    SetNamedScope("outer", Union(GetNamedScope("inner"), Value(Set(3)))).eval
+    // Create 2 sets A and B  in anonymous scope
+    Scope(Assign("A", Value(Set(1)))).eval("")
+    Scope(Assign("B", Value(Set(2)))).eval("")
 
-    // Compute the result of both the scopes
-    assert(GetNamedScope("outer").eval === mutable.HashSet(1, 2, 3))
-    assert(GetNamedScope("inner").eval === mutable.HashSet(1, 2))
+    // Create 2 sets A and B  in a scope called "inner"
+    Scope(Assign("A", Value(Set(1,2)))).eval("inner")
+    Scope(Assign("B", Value(Set(3,4)))).eval("inner")
 
-    // Set an anonymous scope inside a named scope
-    SetNamedScope("scope1", Union(SetAnonScope(Union(Value(Set(5)), Value(Set(10)))), Value(Set(0)))).eval
-    // Compute the result of the named scope
-    assert(GetNamedScope("scope1").eval === mutable.HashSet(0, 10, 5))
+    // Check if Union of set A and B in default scope gives the correct output
+    assert(Union(Variable("A"), Variable("B")).eval() === scala.collection.mutable.HashSet(1, 2, 3, 4, 5, 6))
+
+    // Check if Union of set A and B in anonymous scope gives the correct output
+    assert(Union(Variable("A"), Variable("B")).eval("") === scala.collection.mutable.HashSet(1, 2))
+
+    // Check if Union of set A and B in "inner" scope gives the correct output
+    assert(Union(Variable("A"), Variable("B")).eval("inner") === scala.collection.mutable.HashSet(1, 2, 3, 4))
   }
 }
